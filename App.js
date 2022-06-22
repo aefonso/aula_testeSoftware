065912e56861d6fbc12df8a7dbeeca370b8f2e97
Afonso Henrique <hafonso021@gmail.com>
2022-06-22 11:21:38 -0300

    Update

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ import { Text, View, TextInput, Button  } from 'react-native';
 export default () => {
   return (
     <View>
-      <Text> Digite um número</Text>
-      <TextInput placeholder="Digite aqui"/>
-      <Button title='Calcular'/>
+      <Text testID='titulo'> Digite um número</Text>
+      <TextInput  testID='campo' placeholder="Digite aqui"/>
+      <Button testID='botao' title='Calcular'/>
     </View>
   )
 }
--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,9 +1,28 @@
 import renderer from 'react-test-renderer'
 import App from '../App'
+import {render} from '@testing-library/react-native'
+import { get } from 'react-native/Libraries/Utilities/PixelRatio'
+
 
 describe('Teste de Snapshoot', () => {
     it('Criação de snapshoot', () => {
         const inicial = renderer.create(<App/>).toJSON();
         expect(inicial).toMatchSnapshot();
+    })
+})
+
+describe('Teste de Inicialização', () => {
+    it('Verificando Inicialização dos Componentes', () => {
+        const{getByText, getByTestId} = render(<App/>);
+
+        const tituloApp = getByTestId('titulo');
+        expect(tituloApp).toBeDefined();
+
+        expect(getByTestId('campo')).toBeDefined();
+
+        expect(getByTestId('botao')).toBeDefined();
+
+        const botao = getByText('Calcular');
+        expect(botao).toBeDefined();
     })
 })
