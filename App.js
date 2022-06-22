96b221e48e98f0429a46b782afc450aa670d71c1
Afonso Henrique <hafonso021@gmail.com>
2022-06-22 09:34:52 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { Text, View, InputText, Button  } from 'react-native';
+
+
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text> Digite um número</Text>
+      <InputText placeholder="Digite aqui"/>
+      <Button title='Calcular'/>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ __tests__/Exemplo-test.js
@@ -0,0 +1,53 @@
+import { TestScheduler } from "jest";
+
+function soma(num1, num2){
+    return num1+num2;
+}
+
+function subtracao(num1, num2){
+    return num1-num2;
+}
+
+function multiplicao(num1, num2) {
+    return num1*num2;
+}
+
+describe ('Testes da função soma', () => {
+    it('Soma com positivos', () => {
+        expect(soma(5,3)).toBe(8);
+    })
+
+    it('Soma com Negativos', () => {
+        expect(soma(-1,-2)).toBe(-3);
+    })
+
+})
+
+describe('Testes da função subtração', () => {
+    it('Subtração com Positivos', () => {
+        expect(subtracao(21,21)).toBe(0)
+    })
+    it('Subtração com Negativos', () => {
+        expect(subtracao(-5,-2)).toBe(-3)
+    })
+    
+})
+
+describe('Testes da função multiplicação', () => {
+    it('Multiplicação com Positivos', () => {
+        expect(multiplicao(2,3)).toBe(6)
+    })
+    it('Multiplicação com Negativos', () => {
+        expect(multiplicao(-2,-2)).toBe(4)
+    })
+    
+
+})
+
+ /*test('Teste de Soma com Positivos:', () => {
+     expect(soma(21,21)).toBe(42);
+ });
+
+ test('Soma com Negativos', () => {
+     expect(soma(-1,-2)).toBe(-3);
+ }); */
